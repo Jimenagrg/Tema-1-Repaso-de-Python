ce3e4e955d152ebfccf1229e4843f5cd304d886a
Jimenagrg <146752335+Jimenagrg@users.noreply.github.com>
2025-09-28 15:12:40 +0000

    Agrega bloque __main__ y ejemplo de uso en Ej_3.py

--- Ej_3.py
+++ Ej_3.py
@@ -13,9 +13,11 @@ def clasificar_numeros(lista):
 
     return pares, impares, negativos
 
-numeros = [10, -3, 7, -8, 0, 15, -2]
-pares, impares, negativos = clasificar_numeros(numeros)
 
-print("Pares:", pares)
-print("Impares:", impares)
-print("Negativos:", negativos)
+if __name__ == "__main__":
+    numeros = [10, -3, 7, -8, 0, 15, -2]
+    pares, impares, negativos = clasificar_numeros(numeros)
+
+    print("Pares:", pares)
+    print("Impares:", impares)
+    print("Negativos:", negativos)
