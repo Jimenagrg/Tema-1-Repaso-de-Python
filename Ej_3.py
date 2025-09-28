67a0203393051e0f3a4a54dc277e314ea41daaec
Jimenagrg <146752335+Jimenagrg@users.noreply.github.com>
2025-09-28 15:01:20 +0000

    Agrega Ej_3.py

---
+++ Ej_3.py
@@ -0,0 +1,21 @@
+def clasificar_numeros(lista):
+    pares = []
+    impares = []
+    negativos = []
+
+    for num in lista:
+        if num < 0:
+            negativos.append(num)
+        if num % 2 == 0:
+            pares.append(num)
+        else:
+            impares.append(num)
+
+    return pares, impares, negativos
+
+numeros = [10, -3, 7, -8, 0, 15, -2]
+pares, impares, negativos = clasificar_numeros(numeros)
+
+print("Pares:", pares)
+print("Impares:", impares)
+print("Negativos:", negativos)
