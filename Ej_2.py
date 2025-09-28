bed57b69b5e99c617eece63f9df85763c610dc82
Jimenagrg <146752335+Jimenagrg@users.noreply.github.com>
2025-09-28 15:12:07 +0000

    Agrega bloque __main__ y ejemplo de uso en Ej_2.py

--- Ej_2.py
+++ Ej_2.py
@@ -1,14 +1,16 @@
-ahorro = input("Introduce la cantidad incial, cantidad menusal, número de meses")
-datos = ahorro.split(",")
-cantidad_inicial = float(datos[0])
-cantidad_mensual = float(datos[1])
-número_meses = int(datos[2])
 
-cantidad_ahorrada = cantidad_inicial + cantidad_mensual * número_meses 
-print("Total ahorrado: {}".format(cantidad_ahorrada))
+if __name__ == "__main__":
+    ahorro = input("Introduce la cantidad incial, cantidad menusal, número de meses: ")
+    datos = ahorro.split(",")
+    cantidad_inicial = float(datos[0])
+    cantidad_mensual = float(datos[1])
+    número_meses = int(datos[2])
 
-if cantidad_ahorrada > 5000:
-    print("Felicidades has superado los 5000€")
-else:
-    print("No has conseguido el objetivo de ahorro :(")
+    cantidad_ahorrada = cantidad_inicial + cantidad_mensual * número_meses 
+    print("Total ahorrado: {}".format(cantidad_ahorrada))
+
+    if cantidad_ahorrada > 5000:
+        print("Felicidades has superado los 5000€")
+    else:
+        print("No has conseguido el objetivo de ahorro :(")
 
